9a262b4ba5dc7fd7a3aa5a93ecff6ee23edd2b8a
epsislow <epsislow>
2021-03-30 21:40:22 +0300

    library test

---
+++ library/Lib.js
@@ -0,0 +1,39 @@
+var lib = {
+  'and': function (x1, x2) {
+    return x1 && x2;
+  },
+  'or': function (x1,x2)   {
+    return x1 || x2;
+  },
+  'not': function (x) {
+    return !x?1:0;
+  },
+  'xor': function(x1,x2) {
+    return x1^ x2;
+  },
+  'nor': function(x1, x2) {
+    return !(x1 | x2)?1:0;
+  },
+  'memory': {
+    data: {},
+    set: function(name, value) {
+      this.data[name] = value;
+    },
+    get: function(name) {
+      return this.data[name];
+    },
+  },
+  'truth': function(op) {
+    var info = {'operation': op};
+    for (var x1 = 0; x1 <= 1; x1++) {
+      for (var x2 = 0; x2 <= 1; x2++) {
+        var title = 'x1='+ x1 + ' x2=' + x2;
+        info[title] = lib[op].apply(this, [x1, x2]);
+      }
+    }
+    
+    console.table(info);
+  }
+}
+
+lib.truth('nor');
