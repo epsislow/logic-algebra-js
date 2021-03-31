6c8768cabe994dfa8c43aa84abe593ed6230648a
unknown <adypatulea@yahoo.com>
2021-03-31 19:45:10 +0300

    small changes

--- library/Lib.js
+++ library/Lib.js
@@ -6,16 +6,16 @@ var lib = {
     return x1 || x2;
   },
   'not': function (x) {
-    return !x?1:0;
+    return !(x);
   },
   'xor': function(x1,x2) {
-    return x1^ x2;
+    return x1 ^ x2;
   },
   'nor': function(x1, x2) {
-    return !(x1 | x2)?1:0;
+    return !(x1 || x2);
   },
   'nand': function(x1,x2) {
-    return !(x1 & x2)?1:0;
+    return !(x1 && x2);
   },
   'memory': {
     data: {},
@@ -29,14 +29,193 @@ var lib = {
   'truth': function(op) {
     var info = {'operation': op};
     for (var x1 = 0; x1 <= 1; x1++) {
-      for (var x2 = 0; x2 <= 1; x2++) {
-        var title = 'x1='+ x1 + ' x2=' + x2;
-        info[title] = lib[op].apply(this, [x1, x2]);
+      if (op == 'not') {
+        var title = 'x1='+ x1;
+        info[title] = lib[op](x1)?1:0;
+      } else if (['and','or','not','nor','nand','xor'].includes(op)) {
+        for (var x2 = 0; x2 <= 1; x2++) {
+          var title = 'x1='+ x1 + ' x2=' + x2;
+          info[title] = lib[op](x1, x2)?1:0;
+        }
+      } else {
+        for (var x2 = 0; x2 <= 1; x2++) {
+          for (var qold = 0; qold <= 1; qold++) {
+            var title = 'x1=' + x1 + ' x2=' + x2 + ' qold=' + qold;
+            var values = lib[op](x1, x2, qold);
+            info[title] = 'q='+ values.q + ' nq='+ values.nq;
+          }
+        }
       }
     }
-    
     console.table(info);
+  },
+  's-r-latch-nand': function(s,r, qold = 0) {
+    q = lib.nand(s,nq = lib.nand(qold,r));
+    return {'q': q?1:0, 'nq': nq?1:0};
+  },
+  'd-latch-nand': function(d, en, qold = 0) {
+    nq = lib.nand(qold, lib.nand(en,lib.not(d)));
+    q = lib.nand(lib.nand(d,en), nq);
+    return {'q': q?1:0, 'nq': nq?1:0};
+  },
+  's-r-latch-nor':  function(s,r, qold = 0) {
+    nq = lib.nor(qold,s);
+    q = lib.nor(r,nq);
+    return {'q': q?1:0, 'nq': nq?1:0};
+  },
+  'd-latch-nor': function(d, en, qold = 0) {
+    nq = lib.nor(qold, lib.and(en,d));
+    q = lib.nor(lib.and(lib.not(d),en), nq);
+    return {'q': q?1:0, 'nq': nq?1:0};
+  },
+  'constructFromMatrix': function (objects,connections) {
+      for(connection in connections) {
+
+      }
+  },
+  'getMatrixFromOp': function(op) {
   }
 }
 
+class LogicOperation {
+  constructor(inputs, outputs) {
+    this.inputs = inputs;
+    this.outputs = outputs;
+  }
+
+  perform(inputs) {
+    return 0;
+  }
+}
+
+class And extends LogicOperation {
+  constructor() {
+    super(2,1);
+  }
+
+  perform(inputs) {
+    return lib.and.apply(this, inputs);
+  }
+}
+
+class Nand extends LogicOperation {
+  constructor() {
+    super(2,1);
+  }
+
+  perform(inputs) {
+    return lib.nand.apply(this, inputs);
+  }
+}
+
+class DLatchNor extends LogicOperation {
+  constructor() {
+    super(3,2);
+  }
+
+  perform(inputs) {
+    return lib['d-latch-nor'].apply(this, inputs);
+  }
+}
+
+
+class LogicOperationInput {
+  constructor(LogicOp, number) {
+    if (!LogicOp instanceof LogicOperation) {
+      throw "LogicOperation object expected";
+    }
+
+    this.logicOp = LogicOp;
+    this.number = number;
+  }
+
+  connectToOutput(LogicOpOutput) {
+    if (!LogicOpOutput instanceof LogicOperationOutput) {
+      throw "LogicOperationOutput object expected";
+    }
+    if (this.connectedToOutput == LogicOpOutput) {
+      console.log('done');
+      return;
+    }
+    this.connectedToOutput = LogicOpOutput;
+    LogicOpOutput.connectToInput(this);
+  }
+}
+
+class LogicOperationOutput {
+  constructor(LogicOp, number) {
+    if (!LogicOp instanceof LogicOperation) {
+      throw "LogicOperation object expected";
+    }
+
+    this.logicOp = LogicOp;
+    this.number = number;
+  }
+
+  connectToInput(LogicOpInput) {
+    if (!LogicOpInput instanceof LogicOperationInput) {
+      throw "LogicOperationInput object expected";
+    }
+    if (this.connectedToInput == LogicOpInput) {
+      console.log('done');
+      return;
+    }
+    this.connectedToInput = LogicOpInput;
+    LogicOpInput.connectToOutput(this);
+  }
+}
+
+class ConnectorMatrix {
+  constructor() {
+    this.objects = [];
+    this.inputs = [];
+    this.outputs = [];
+  }
+
+  addLogicOp(LogicOp, name = '') {
+    if (!LogicOp instanceof LogicOperation) {
+      throw "LogicOperation object expected";
+    }
+
+    if (name) {
+      this.objects[name] = LogicOp;
+    } else {
+      this.objects[this.objects.length] = LogicOp;
+    }
+
+    for(var i = 0; i < LogicOp.inputs; i++) {
+      this.inputs[this.inputs.length] = new LogicOperationInput(LogicOp, i);
+    }
+
+    for(var i = 0; i < LogicOp.outputs; i++) {
+      this.outputs[this.outputs.length] = new LogicOperationOutput(LogicOp, i);
+    }
+    return this;
+  }
+}
+
+var Connector = new ConnectorMatrix();
+
+Connector
+    .addLogicOp(new And(), 'and1')
+    .addLogicOp(new And(), 'and2');
+
+
+var dlatch = new DLatchNor();
+
+console.log(dlatch.perform([x1=0, x2=0, qold=1]));
+
+// -----------------
+
+lib.truth('and');
+lib.truth('or');
+lib.truth('not');
 lib.truth('nand');
+lib.truth('nor');
+lib.truth('xor');
+
+
+lib.truth('s-r-latch-nand');
+lib.truth('d-latch-nand');
+lib.truth('s-r-latch-nor');
+lib.truth('d-latch-nor');
