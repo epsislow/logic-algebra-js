996736a8bda25e73635440fafb03d2738cf06f63
epsislow <epsislow>
2021-04-03 01:53:47 +0300

    node

--- library/Node.js
+++ library/Node.js
@@ -1,8 +1,30 @@
 class Node{
-  constructor(type) {
+  constructor(type, owner) {
     if (!['in', 'out', 'both'].includes(type)){
       throw "Type not in,out or both";
     }
     this.type= type;
+    if (owner instanceof LogicOperation) {
+      this.owner=owner;
+    }
+  }
+  
+  connect(node) {
+    if (node instanceof LogicOperation) {
+      if(this.type =='in' && node.type !='in'){
+        this.connectedTo= node.owner;
+        if(this.from!=node){
+          this.from=node;
+          node.connect(this);
+        }
+      } else if(this.type =='out' && node.type !='out') {
+        this.connectedTo= node.owner;
+        if(this.to!=node) {
+          this.to=node;
+          node.connect(this);
+        }
+      }
+    }
   }
 }
+
