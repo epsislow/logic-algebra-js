7a4b714f4885d41ea57270fc6ed4403b8efccd15
epsislow <epsislow>
2021-05-15 14:39:30 +0300

    bb

---
+++ library/sha1/Random.js
@@ -0,0 +1,20 @@
+$('document').ready(function(document){
+
+console.log('Random v0.1.0 [rd]');
+
+window.rd = function() {
+  this.randomBytes = function (length) {
+    var result = [];
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result.push(characters.charAt(Math.floor(Math.random() *
+        charactersLength)));
+    }
+    return result.join('');
+  }
+  
+  
+}
+
+});
