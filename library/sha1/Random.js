06e04d44eabddf1e70c7d6dd044d04a77b1c4970
epsislow <epsislow>
2021-05-29 21:48:20 +0300

    hh

--- library/sha1/Random.js
+++ library/sha1/Random.js
@@ -1,8 +1,6 @@
-$('document').ready(function(document){
-
 console.log('Random v0.1.0 [rd]');
 
-window.rd = (function () {
+var rd = (function () {
   var pub = {};
   
   pub.randomBytes = function (length, letters=3, num= 0, symbols= 0) {
@@ -21,8 +19,28 @@ window.rd = (function () {
     return result.join('');
   }
   
-  pub.rand= function() {
-    
+  pub.rand= function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+  pub.hashCode = function (str, seed = 0, b = 32) {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+    return (4294967296 * (2097151 & h2) + (h1 >>> 0)).toString(b);
+}
+  
+  pub.getUniqueInObj= function(name, obj, i = 0) {
+    if (name + i in obj) {
+      return getUniqueInObj(name, obj, rand(1, 1000));
+    }
+    return name + i;
   }
   
   pub.algs = function () {
@@ -40,7 +58,7 @@ window.rd = (function () {
       return (t >>> 0) / 4294967296;
     }
     
-    pb.xmur = function (str) {
+  pub.xmur = function (str) {
       for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
         h = Math.imul(h ^ str.charCodeAt(i), 3432918353),
         h = h << 13 | h >>> 19;
@@ -57,8 +75,12 @@ window.rd = (function () {
   return pub;
 })();
 
-});
 
+$('document').ready(function(document){
+  if (typeof window != 'undefined') {
+  	window.rd = rd;
+  }
+});
 
 
 //javascript:(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
