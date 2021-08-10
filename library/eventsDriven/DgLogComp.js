70dfc4f2ef8d476eeafe5203e0b251c0b29a3b77
epsislow <epsislow>
2021-08-11 00:02:43 +0300

    er

--- library/eventsDriven/DgLogComp.js
+++ library/eventsDriven/DgLogComp.js
@@ -1,14 +1,34 @@
 comp = function () {
+  objChanged = function(a, b) {
+     for(const [k,v] of Object.entries(a))
+     {
+       return (!b.hasOwnProperty(k)) || (v!=b[k]);
+     }
+  }
+  
+  evalThis = function(inStates) {
+    
+  }
   
   var pub= {
     inStates: {},
     states: {},
     eval: function(iss) {
-      if(!hasChangeIn(iss, pub.inStates)) {
+      if(!objChanged(iss, pub.inStates)) {
         return pub.states;
       }
       pub.inStates = iss;
       return pub.states = evalThis(iss);
+    },
+    trigger: function(iss) {
+      var old = {...pub.states};
+      pub.eval(iss);
+      if(objChanged(pub.states, old)) {
+        outConns.forEach(function(pinKey) {
+          var comp=getCompByPinKey(pinKey);
+          comp.trigger()
+        });
+      }
     }
   }
   return pub;
