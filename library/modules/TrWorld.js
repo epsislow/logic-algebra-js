598cb2b494bf3b5e6d517e00cd32e97d99e3ae16
epsiSlow <epsislow@gmail.com>
2021-08-31 23:42:46 +0300

    test

---
+++ library/modules/TrWorld.js
@@ -0,0 +1,77 @@
+/*
+ Module: TrWorld
+ Version: 1.0.1
+ Author: epsislow@gmail.com
+*/
+
+//-a-b-c-
+
+var TrUnit = (function (genes) {
+	var pub = {};
+	
+	pub.genes = genes;
+	
+	pub.nextGenes = function (genesWith) {
+		var fn = TrWorld.getCrossOverHowFromGenes(genes);
+		return fn(genes, genesWith);
+	}
+	
+	pub.nextUnit = function (unit) {
+		var newGenes = this.nextGenes(unit.genes);
+		return TrWorld.genesToUnit(newGenes);
+		
+		
+	}
+	
+	return pub;
+});
+
+var TrWorld = (function () {
+	
+	var pub = {};
+	
+	pub.genesToUnit = function () {
+		
+	};
+	
+	pub.unitToGenes = function () {
+	};
+	
+	pub.getCrossOverHowFromGenes = function (genes) {
+	};
+	
+	pub.gen = {
+		firstUnit: function() {
+		},
+		nextUnit: function () {
+		},
+		randomUnit: function () {
+		},
+		next: function () {
+			var gen = [];
+			while(let unit = this.nextUnit();let randomUnit = gen.randomUnit()) {
+				gen.push(unit.nextUnit());
+			}
+		}
+	}
+	
+	pub.unit = {
+		attack: function (unit) {
+		},
+		defence: function (unit) {
+		},
+		dinner: function (unit) {
+		}
+		nomeal: function (unit) {
+		},
+	};
+	
+	pub.surviveGen
+	
+	
+	
+	return pub;
+});
+
+
+export { TrWorld, TrUnit }
