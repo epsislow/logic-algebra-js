c8eec5380fa52fa2a16b42d5ecd6a3ea273702aa
epsislow <epsislow>
2021-09-06 13:44:39 +0300

    bb

---
+++ library/modules/NBQueue.js
@@ -0,0 +1,17 @@
+/*
+ Module: NBQueue
+ Version: 1.0.1
+ Author: epsislow@gmail.com
+*/
+
+var NBQueue = (function () {
+	const pub = {};
+	
+	pub.add = function(hdl) {
+	  
+	}
+	
+	return pub;
+})();
+
+export { NBQueue }
