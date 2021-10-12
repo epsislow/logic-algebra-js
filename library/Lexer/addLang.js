8fdf5c59aa21d57c9a409dc7654482e28ed6c710
epsislow <epsislow>
2021-10-13 00:54:37 +0300

    hh

--- library/Lexer/addLang.js
+++ library/Lexer/addLang.js
@@ -46,8 +46,8 @@ var lex = (function() {
   
   pub.parse=function(txt) {
     const ast= [];
-	const cursor=0;
-	const flags = {};
+	var cursor=0;
+	const flags = {type:0};
 	const error = {code: 0, description: ''};
 	
 	function setError(code, description) {
@@ -61,19 +61,87 @@ var lex = (function() {
 		throw new Error('[' + error.code + '] ' + error.description);
 	}
 
+  function pushTypeToAst(only=['var','number','op','assign']) {
+    if (flags.type == 'assign' && only.includes('assign')) {
+      ast.push({ assign: '=' })
+    } else if (flags.type == 'op' && only.includes('op')) {
+      ast.push({ op: flags.op })
+      delete flags.op;
+    } else if (flags.type == 'var' && only.includes('var')) {
+      ast.push({ varLoad: flags.var });
+      delete flags.var;
+    } else if (flags.type == 'number' && only.includes('number')) {
+      ast.push({ number: flags.number });
+      delete flags.number;
+    }
+  }
 
 	function expectation() {
 		const token = txt.charAt(cursor);
 		
-		return setError(0, 'Reached unimplemented area!');
+	//	console.log(token);
+		
+		if(!flags.type || ['assign','op','var'].includes(flags.type)) {
+		  if((/[A-Z]/i).test(token)) {
+		 pushTypeToAst(['assign','op']);
+		    flags.type='var';
+		    if(!flags.var) {
+		      flags.var= ''+token;
+		    } else {
+		      flags.var+=token;
+		    }
+		  }
+		}
+		
+		if(!flags.type || ['op','assign','number','var'].includes(flags.type)) {
+		  if((/[0-9]/).test(token)) {
+		    pushTypeToAst(['assign','op'])
+		    if(!flags.type || ['op','assign'].includes(flags.type)) {
+		      flags.type='number';
+		    }
+		    if(flags.type=='number') {
+		     if (!flags.number) {
+		       flags.number = ''+token;
+		     } else {
+		       flags.number += token;
+		     }
+		    } else if (flags.type =='var') {
+		      if (!flags.var) {
+		        flags.var = '' + token;
+		      } else {
+		        flags.var += token;
+		      }
+		    }
+		    
+		  }
+		}
+		
+		if(flags.type) {
+		  if((/[-+*/]/).test(token)) {
+		    if(flags.type!='op') {
+		      pushTypeToAst(['var','number']);
+		      flags.type='op';
+		    }
+		    flags.op=token;
+		  }
+		  if(flags.type=='var' && token=='=')
+		  {
+		    ast.push({varAssign:flags.var});
+		    delete flags.var;
+		    flags.type='assign';
+		  }
+		}
+		
+		//return setError(0, 'Reached unimplemented area!');
 		
 		return 1;
 	}
 	
-    
-	while(expectation()) {
-		cursor++;
-		if (cursor>=txt.length) {
+	while(cursor+=expectation()) {
+		if (cursor>txt.length) {
+		  if(flags.type) {
+		    pushTypeToAst();
+		  }
 			return ast;
 		}
 	}
@@ -85,4 +153,4 @@ var lex = (function() {
 })();
 
 
-export { lex  as Lex }
+export { lex as Lex }
