d45a92459102d9fd652587debbb8968e7c4fbef6
epsislow <epsislow>
2022-02-20 12:06:28 +0200

    nn

--- library/minigm/gam2b.js
+++ library/minigm/gam2b.js
@@ -19,6 +19,7 @@ window.r = r;
             .addButton('Cache', function () { gam2.mem.loadData(); }, 'button btn-success')
             .addButton('Load', function () { gam2.mem.loadSlot(); },'button btn-info')
             .addButton('Save', function () { gam2.mem.saveSlot(); }, 'button btn-danger')
+            .addButton('+++', function () { gam2.idle.calcSec(3600); console.log('+3600') },'button btn-light')
             .addText(' ').el;
 
         vs.addSectionsToMain();
