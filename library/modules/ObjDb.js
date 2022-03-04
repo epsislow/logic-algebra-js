a4b890ef75f160101e91c9eab0be02bd88059f13
epsislow <epsislow>
2022-03-05 00:23:53 +0200

    yy

---
+++ library/modules/ObjDb.js
@@ -0,0 +1,50 @@
+/*
+ Module: ObjDb
+ Version: 1.0.1
+ Author: epsislow@gmail.com
+*/
+
+var ObjDb = (function () {
+    let pub;
+    pub = {
+      'addDb': function() {
+        
+      },
+      'addList': function() {
+        
+      },
+      'useDb': function() {
+        
+      },
+      'select': function() {
+        
+      },
+      'insert': function() {
+        
+      },
+      'update': function() {
+        
+      },
+      'delete': function() {
+        
+      },
+      'truncate': function() {
+        
+      }
+    }
+    return pub;
+})();
+
+var ObjRef = (function() {
+  let pub;
+  pub = {
+    'new': function() {
+      
+    }
+  }
+  
+  return pub;
+})();
+
+
+export { ObjDb, ObjRef }
