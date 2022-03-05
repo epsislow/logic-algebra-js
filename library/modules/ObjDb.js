03581c7478d7b13bbd16aaa2e5f7626493059a4e
epsislow <epsislow>
2022-03-05 10:03:34 +0200

    hh

--- library/modules/ObjDb.js
+++ library/modules/ObjDb.js
@@ -1,31 +1,41 @@
 /*
  Module: ObjDb
  Version: 1.0.1
+ t/tttjrjfjjgjcjdjzjsjdjuvudjsjjcjieiiviviieidigieiivugiie
  Author: epsislow@gmail.com
 */
 
 var ObjDb = (function () {
     let pub;
     pub = {
-      'addDb': function() {
+      'db': {},
+      'pre': {'db':0},
+      'addDb': function(name) {
+        this.db[name]={};
         return 1;
       },
-      'addList': function() {
-        
+      'addList': function(db, name) {
+        this.db[db][name]={id:0};
       },
-      'useDb': function() {
-        
+      'useDb': function(db) {
+        this.pre.db=db;
       },
       'select': function() {
         
       },
-      'insert': function() {
-        
+      'insert': function(list, val, id=0) {
+        let dbl=this.db[this.pre.db][list];
+        let nid=id?id:dbl.id; 
+        dbl[nid] = val;
       },
-      'update': function() {
-        
+      'update': function(list, val, id = 0) {
+        let dbl = this.db[this.pre.db][list];
+        let nid = id ? id : dbl.id;
+        dbl[nid] = val;
       },
-      'delete': function() {
+      'deleteId': function(list, nid) {
+        let dbl = this.db[this.pre.db][list];
+        delete dbl[nid];
         
       },
       'truncate': function() {
