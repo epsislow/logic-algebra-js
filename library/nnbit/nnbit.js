967b1eac66d425187b0a7e9ca09e168d52ecda51
epsislow <epsislow>
2022-03-13 20:13:02 +0200

    nnbit

---
+++ library/nnbit/nnbit.js
@@ -0,0 +1,9 @@
+import { DbStorageConstr } from "/library/modules/DbStorage.js";
+
+var NnBit = (function (DbStrConstr) {
+  let pub = {};
+  
+  return pub;
+})(DbStorageConstr);
+
+export { NnBit }
