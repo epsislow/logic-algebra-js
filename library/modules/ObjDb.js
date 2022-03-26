03987d21157b953082c89bb75c751febfa8efcfd
epsislow <epsislow>
2022-03-26 09:48:55 +0200

    we

--- library/modules/ObjDb.js
+++ library/modules/ObjDb.js
@@ -77,6 +77,12 @@ var ObjSimple = (function() {
     },
     'addFn': function(o) {
       let f= {};
+      /*
+      let handlers = {
+        set
+      }*/
+      //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+      //let o= new Proxy(o, handlers);
       let pub = {'o':o, 'f': f};
       f.o=o;
       f.trz = {old: 0, en:0};
@@ -108,6 +114,7 @@ var ObjSimple = (function() {
       }
       f.add=function(name, idStart=0) {
         pub.o[name] = {id: idStart};
+        f.event('f.add', f.ref(name, idStart) );
         return f;
       }
       f.ref=function(name, id) {
@@ -119,10 +126,13 @@ var ObjSimple = (function() {
       f.new=function(name, prop = {}) {
         let nid= pub.o[name].id++;
         pub.o[name][nid] = prop;
+        f.event('f.new', f.ref(name, nid));
         return pub.o[name][nid];
       }
       f.remove= function(name,id) {
+        let od = pub.o[name][id];
         delete pub.o[name][id];
+        f.event('f.remove', {od: od, name:name, id:id});
         return this;
       }
       f.removeRef=function(ref) {
@@ -157,8 +167,10 @@ var ObjSimple = (function() {
 
         return l;
       })(f);
+      let isRef = function(ref) {
+        return (Array.isArray(ref) && ref.length === 2);
+      }
       let has = function(it, where={}) {
-        
         for(let i in it) {
           if(!where.hasOwnProperty(i)) {
             return 0;
@@ -168,7 +180,43 @@ var ObjSimple = (function() {
           }
         }
       }
-    
+      f.event= function(act, evob) {
+        switch(act) {
+          case 'f.add': 
+            
+            break;
+          case 'f.new':
+            if(isRef(evob)) {
+              let name = evob[0];
+              let id = evob[1];
+              if(name in f.filter) {
+                let it = f.get(evod);
+                
+                for(let i in f.filter[name]) {
+                  if(has(it, f.filter[name][i].where)) {
+                    f.filter[name][i][id] = id;
+                  }
+                }
+                
+              }
+            }
+            break;
+          case 'f.remove':
+            let name=evod.name;
+            let id=evod.id;
+            let it=evod.od;
+            if(name in f.filter) {
+              for(let i in f.filter[name]) {
+                if(id in f.filter[name][i]) {
+                  delete f.filter[name][i][id];
+                }
+              }
+            }
+            break;
+          case 'f.update':
+            break;
+        }
+      }
       f.filter= (function (f) {
         let c= {};
         c.add=function(name, ofName, where={}) {
@@ -184,6 +232,8 @@ var ObjSimple = (function() {
             }
           }
           
+        }
+          
           /*
           f.event[ofName].onAdd= (function(cc, where) {
             return function(li, i) {
@@ -196,7 +246,8 @@ var ObjSimple = (function() {
           f.event[ofName].onRemove = function(name, i) {
             c.list[name]
           }*/
-        }
+        
+        
         
         return c;
       })(f);
