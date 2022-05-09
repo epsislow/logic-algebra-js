d5cede0c2ee06de5e401b9c88a780631c3216d03
epsi Slow <adypatulea@yahoo.com>
2022-05-09 18:56:52 +0300

    test

--- library/modules/layerJ.js
+++ library/modules/layerJ.js
@@ -5,7 +5,31 @@
 */
 
 var layerJ = (function () {
+  let pub = {};
 
-});
+  pub.node = {
+    'add': function (value = 0, spikeRequiredValue = 10, spikeTopValue = 20)
+    {
+      return {
+        value: value ? value: Math.round(Math.random()*10) + 1,
+        spikeRequiredValue: spikeRequiredValue,
+        spikeTopValue: spikeTopValue,
+      };
+    }
+  };
+
+  pub.layer = {
+      'add': function (layers = [], nodes = 20, nodeConfig = [])
+      {
+        let layer = new Array(nodes).fill( pub.node.add.apply(null, nodeConfig));
+        layers.push(layer);
+        return layers;
+      }
+  };
+
+
+
+  return pub;
+})();
 
 export { layerJ }
