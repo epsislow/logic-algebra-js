0a34bdcee48547420f75480231258629993ea794
epsislow <epsislow>
2026-01-17 23:07:25 +0200

    update main.js

--- library/logTscript/lib/main.js
+++ library/logTscript/lib/main.js
@@ -1,3 +1,17 @@
+function loadScript(url)
+{    
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    head.appendChild(script);
+}
+
+function loadJs(name) {
+  loadScript('lib/'+ name);
+}
+
+
 /* ================= TOKENIZER ================= */
 
 class Token {
